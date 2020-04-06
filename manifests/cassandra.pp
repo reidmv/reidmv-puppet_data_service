0733230351b122c7bf6bfd4ce063bbf6e540a807
Reid Vandewiele <reid@puppet.com>
2020-04-06 14:17:33 -0700

    Add parameters for listen_address, seeds
    Seeds gets a new default and listen_address is made configurable.

--- manifests/cassandra.pp
+++ manifests/cassandra.pp
@@ -1,6 +1,7 @@
 class puppet_data_service::cassandra (
-  String $seeds,
-  String $dc = 'DC1',
+  String $seeds          = getvar('facts.ipaddress'),
+  String $listen_address = getvar('facts.ipaddress'),
+  String $dc    = 'DC1',
 ) {
 
   class { 'cassandra::apache_repo':
@@ -9,23 +10,24 @@ class puppet_data_service::cassandra (
   }
 
   class { 'cassandra':
-    package_name => 'cassandra',
-    dc           => $dc,
-    settings     => {
-      'cluster_name' => 'PuppetDataCluster',
-      'endpoint_snitch' => 'GossipingPropertyFileSnitch',
-      'commitlog_directory' => '/var/lib/cassandra/commitlog',
-      'hints_directory' => '/var/lib/cassandra/hints',
-      'saved_caches_directory' => '/var/lib/cassandra/saved_caches',
-      'commitlog_sync' => 'periodic',
+    package_name                    => 'cassandra',
+    dc                              => $dc,
+    settings                        => {
+      'cluster_name'                => 'PuppetDataCluster',
+      'endpoint_snitch'             => 'GossipingPropertyFileSnitch',
+      'commitlog_directory'         => '/var/lib/cassandra/commitlog',
+      'hints_directory'             => '/var/lib/cassandra/hints',
+      'saved_caches_directory'      => '/var/lib/cassandra/saved_caches',
+      'commitlog_sync'              => 'periodic',
       'commitlog_sync_period_in_ms' => '10000',
-      'num_tokens' => 256,
-      'partitioner' => 'org.apache.cassandra.dht.Murmur3Partitioner',
-      'start_native_transport' => true,
-      'data_file_directories' => [
+      'num_tokens'                  => 256,
+      'partitioner'                 => 'org.apache.cassandra.dht.Murmur3Partitioner',
+      'start_native_transport'      => true,
+      'listen_address'              => $listen_address,
+      'data_file_directories'       => [
         '/var/lib/cassandra/data'
       ],
-      'seed_provider' => [{
+      'seed_provider'               => [{
         'class_name' => 'org.apache.cassandra.locator.SimpleSeedProvider',
         'parameters' => [{
           'seeds' => $seeds,
