0ac10d7765338e5cc2a124643a695113c3076a9b
Reid Vandewiele <reid@puppet.com>
2020-04-07 10:49:35 -0700

    Ensure full dependency list for puppetserver
    Make sure not to edit the puppet.conf file until all required changes
    have been successfully applied.

--- manifests/puppetserver.pp
+++ manifests/puppetserver.pp
@@ -1,47 +1,45 @@
 class puppet_data_service::puppetserver {
 
-  # Build dependencies
-  ['make', 'automake', 'gcc', 'gcc-c++', 'kernel-devel'].each |$package| {
-    package { $package:
-      ensure => present,
-      before => [
-        Package['puppet_gem cassandra-driver'],
-        Package['puppetserver_gem cassandra-driver'],
-      ],
-    }
-  }
-
-  ['puppet_gem', 'puppetserver_gem'].each |$provider| {
-    package { "${provider} cassandra-driver":
-      name     => 'cassandra-driver',
-      ensure   => present,
-      provider => $provider,
-    }
-  }
-
-  file { '/etc/puppetlabs/puppet/get-nodedata.rb':
-    ensure => file,
-    owner  => 'pe-puppet',
-    group  => 'pe-puppet',
-    mode   => '0755',
-    source => 'puppet:///modules/puppet_data_service/get-nodedata.rb',
-  }
-
   $hosts = puppetdb_query(@(PQL)).map |$rsrc| { $rsrc['certname'] }.sort
     resources[certname] {
       type = "Class" and
       title = "Puppet_data_service::Cassandra" }
     | PQL
 
-  file { '/etc/puppetlabs/puppet/puppet-data-service.yaml':
-    ensure  => file,
-    owner   => 'pe-puppet',
-    group   => 'pe-puppet',
-    mode    => '0640',
-    content => epp('puppet_data_service/puppet-data-service.yaml.epp', {
-      hosts => $hosts
-    }),
-  }
+  $gem_build_dependencies = (
+    package { ['make', 'automake', 'gcc', 'gcc-c++', 'kernel-devel']:
+      ensure => present,
+    }
+  )
+
+  $resource_dependencies = flatten([
+    ['puppet_gem', 'puppetserver_gem'].map |$provider| {
+      package { "${provider} cassandra-driver":
+        name     => 'cassandra-driver',
+        ensure   => present,
+        provider => $provider,
+        require  => $gem_build_dependencies,
+      }
+    },
+
+    file { '/etc/puppetlabs/puppet/get-nodedata.rb':
+      ensure => file,
+      owner  => 'pe-puppet',
+      group  => 'pe-puppet',
+      mode   => '0755',
+      source => 'puppet:///modules/puppet_data_service/get-nodedata.rb',
+    },
+
+    file { '/etc/puppetlabs/puppet/puppet-data-service.yaml':
+      ensure  => file,
+      owner   => 'pe-puppet',
+      group   => 'pe-puppet',
+      mode    => '0640',
+      content => epp('puppet_data_service/puppet-data-service.yaml.epp', {
+        hosts => $hosts
+      }),
+    },
+  ])
 
   pe_ini_setting { 'puppetserver puppetconf trusted external script':
     ensure  => present,
@@ -49,5 +47,6 @@ class puppet_data_service::puppetserver {
     setting => 'trusted_external_command',
     value   => '/etc/puppetlabs/puppet/get-nodedata.rb',
     section => 'master',
+    require => $resource_dependencies,
   }
 }
