c82bccc6c78be36623ce1a3766b86533eb40c727
Reid Vandewiele <reid@puppet.com>
2020-06-17 09:50:00 -0700

    Refactor environment and module schema
    Keep modules in a separate table indexed on environment,name. This makes
    it easier for users to interact with and visualize the data when using
    tools like TablePlus or other generic Cassandra utilities.

--- files/get-r10k-environments.rb
+++ files/get-r10k-environments.rb
@@ -13,23 +13,34 @@ class PuppetDataClient
   end
 
   def get_r10k_environments
-    statement = @session.prepare('SELECT JSON * FROM puppet_environments')
-    results   = @session.execute(statement)
+    env_results = @session.execute('SELECT JSON * FROM environments')
+    mod_results = @session.execute('SELECT JSON * FROM modules')
 
-    # Transform JSON formatted result into a Ruby hash
-    environments = results.map do |result|
-      data = JSON.parse(result['[json]'])
-      data['modules'] = if data['modules']
-                          data['modules'].reduce({}) do |memo,(key,val)|
-                            memo.tap { |mem| mem[key] = JSON.parse(val) }
-                          end
-                        else
-                          {}
-                        end
-      [data.delete('name'), data]
-    end.to_h
+    # Transform JSON formatted array of module results into a nested Ruby hash
+    modules = mod_results.map { |res| JSON.parse(res['[json]']) }
+                         .group_by { |res| res['environment'] }
+                         .reduce({}) do |e_memo,(env,mods)|
+                           mod_hash = mods.reduce({}) do |m_memo,m_data|
+                             m_data.delete('environment')
+                             m_memo[m_data.delete('name')] = m_data
+                             m_memo
+                           end
+                           e_memo[env] ||= {}
+                           e_memo[env] = e_memo[env].merge(mod_hash)
+                           e_memo
+                         end
 
-    # Transform data to R10k format
+    # Transform JSON formatted environment results into a Ruby hash with
+    # attached modules
+    environments = env_results.map { |res| JSON.parse(res['[json]']) }
+                              .reduce({}) do |memo,data|
+                                name = data.delete('name')
+                                data['modules'] = modules[name] || {}
+                                memo[name] = data
+                                memo
+                              end
+
+    # Transform beautiful data to ugly R10k format
     environments.reduce({}) do |e_memo,(e_name,e_data)|
       e_data['remote'] = e_data.delete('source')
       e_data['ref']    = e_data.delete('version')
@@ -38,10 +49,10 @@ class PuppetDataClient
         m_data['git'] = m_data.delete('source')
         m_data['ref']    = m_data.delete('version')
 
-        m_data.delete('type')
+        type = m_data.delete('type')
 
-        # If there's a source, save as hash. Otherwise, save as version (forge)
-        m_memo[m_name] = m_data['git'] ? m_data.compact : m_data['ref']
+        # If it's a git module, save as hash. Otherwise, save as version (forge)
+        m_memo[m_name] = (type == 'git') ? m_data.compact : m_data['ref']
         m_memo
       end
 
