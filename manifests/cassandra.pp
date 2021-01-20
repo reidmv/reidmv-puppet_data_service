10b5677d536d9223245353ae6b119a58e911d2c0
Reid Vandewiele <reid@puppet.com>
2021-01-20 11:55:43 -0800

    Remove unnecessary Cassandra patch
    Cassandra 3.11.9 is now available, which is newer than the required 3.11.7

--- manifests/cassandra.pp
+++ manifests/cassandra.pp
@@ -5,50 +5,6 @@ class puppet_data_service::cassandra (
   String $storage_port        = '7000',
   String $max_heap_size_in_mb = '256',
 ) {
-
-  # BUG https://issues.apache.org/jira/browse/CASSANDRA-15273
-  # Remove as soon as Cassandra 3.11.7 or newer is released
-  package { 'patch':
-    ensure => present,
-    before => Exec['patch cassandra startup file'],
-  }
-  exec { 'patch cassandra startup file':
-    unless  => '/bin/grep -q runuser /etc/rc.d/init.d/cassandra',
-    require => Package['cassandra'],
-    notify  => Service['cassandra'],
-    command => @(PATCH),
-      /bin/patch /etc/rc.d/init.d/cassandra <<'EOF'
-      diff --git a/redhat/cassandra b/redhat/cassandra
-      index 677ff8c7ff..97a0447435 100644
-      --- a/redhat/cassandra
-      +++ b/redhat/cassandra
-      @@ -69,15 +69,16 @@ case "$1" in
-               echo -n "Starting Cassandra: "
-               [ -d `dirname "$pid_file"` ] || \
-                   install -m 755 -o $CASSANDRA_OWNR -g $CASSANDRA_OWNR -d `dirname $pid_file`
-      -        su $CASSANDRA_OWNR -c "$CASSANDRA_PROG -p $pid_file" > $log_file 2>&1
-      +        runuser -u $CASSANDRA_OWNR -- $CASSANDRA_PROG -p $pid_file > $log_file 2>&1
-               retval=$?
-      +        chown root.root $pid_file
-               [ $retval -eq 0 ] && touch $lock_file
-               echo "OK"
-               ;;
-           stop)
-               # Cassandra shutdown
-               echo -n "Shutdown Cassandra: "
-      -        su $CASSANDRA_OWNR -c "kill `cat $pid_file`"
-      +        runuser -u $CASSANDRA_OWNR -- kill `cat $pid_file`
-               retval=$?
-               [ $retval -eq 0 ] && rm -f $lock_file
-               for t in `seq 40`; do
-      --
-      2.25.0
-      EOF
-      chmod a+x /etc/rc.d/init.d/cassandra
-      /bin/systemctl daemon-reload
-      | PATCH
-  }
-
   class { 'cassandra::apache_repo':
     release => '311x',
     before  => Class['cassandra'],
